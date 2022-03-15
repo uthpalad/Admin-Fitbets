9efc7d5c83760ff7056445785bed22056d11d4d8
BhagyaAriyadasa <57471696+BhagyaAriyadasa@users.noreply.github.com>
2022-03-15 12:00:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/DashBoard/dashboard";
+import NewCategory from "./components/Category/addCategory";
+import Home from "./components/Home";
+import CategoryList from "./components/Category/CategoryList";
+import NewSubCategory from "./components/Subcategory/addSubCategory";
+import EditCategory from "./components/Category/editCategory";
+import SubCategoryList from "./components/Subcategory/subCategoryList";
+import EditSubCategory from "./components/Subcategory/editSubCategory";
+import NewObjective from "./components/Objective/addObjective";
+import ObjectiveList from "./components/Objective/objectiveList";
 
-
-import Dashboard from "./Components/DashBoard/dashboard";
-import NewCategory from "./Components/Category/addCategory";
-import Home from "./Components/home";
-import CategoryList from "./Components/Category/categoryList";
-import NewSubCategory from "./Components/SubCategory/addSubCategory";
-import EditCategory from "./Components/Category/editCategory";
-import SubCategoryList from "./Components/SubCategory/subCategoryList";
-import EditSubCategory from "./Components/SubCategory/editSubCategory";
-import NewObjective from "./Components/Objective/addObjective";
-import ObjectiveList from "./Components/Objective/objectiveList";
 function App() {
   return (
     <div>
@@ -22,8 +21,14 @@ function App() {
           <Route path="/all_category" element={<CategoryList />}></Route>
           <Route path="/edit_category" element={<EditCategory />}></Route>
           <Route path="/sub_category" element={<NewSubCategory />}></Route>
-          <Route path="/all__sub_category" element={<SubCategoryList />}></Route>
-          <Route path="/edit_sub_category" element={<EditSubCategory />}></Route>
+          <Route
+            path="/all__sub_category"
+            element={<SubCategoryList />}
+          ></Route>
+          <Route
+            path="/edit_sub_category"
+            element={<EditSubCategory />}
+          ></Route>
           <Route path="/objective" element={<NewObjective />}></Route>
           <Route path="/objective_list" element={<ObjectiveList />}></Route>
         </Routes>
