d8944266da5842db68b0c596397aceb0ebaf3e34
Abesekara-AWADTM <thiliniabesekara1998@gmail.com>
2022-03-15 09:13:36 -0700

    objectives

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import NewCategory from "./Components/Category/addCategory";
 import Home from "./Components/home";
 import CategoryList from "./Components/Category/categoryList";
 import NewSubCategory from "./Components/SubCategory/addSubCategory";
-import UserDetails from "./Components/UserDetails/userDetails";
-import UserList from "./Components/UserList/UserList";
-import SubCategoryList from "./Components/SubCategory/subCategoryList";
-
+import Objective from "./Components/Objectives/addObjective"
 function App() {
   return (
     <div>
@@ -18,10 +15,9 @@ function App() {
           <Route path="/" element={<Home />}></Route>
           <Route path="/category" element={<NewCategory />}></Route>
           <Route path="/all_category" element={<CategoryList />}></Route>
-          <Route path="/all_subcategory" element={<SubCategoryList />}></Route>
-          <Route path="/userlist" element={<UserList />}></Route>
+
           <Route path="/sub_category" element={<NewSubCategory />}></Route>
-          <Route path="/userDetails" element={<UserDetails />}></Route>
+          <Route path="/add_objectives" element={<Objective/>}></Route>
         </Routes>
       </Router>
     </div>
